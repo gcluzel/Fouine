f9e1f5a0bf29ae134450cc0025a7fb80bba76348
jdevevey <julien.devevey@ens-lyon.fr>
2017-04-05 11:56:16 +0200

    ok

---
+++ _build/parser.mli
@@ -0,0 +1,29 @@
+type token =
+  | Int of (int)
+  | Var of (string)
+  | Let
+  | In
+  | Fun
+  | PrInt
+  | Right_arrow
+  | False
+  | True
+  | Let_rec
+  | If
+  | Then
+  | Else
+  | Plus
+  | Times
+  | Minus
+  | C_eq
+  | C_ge
+  | C_neq
+  | C_g
+  | C_l
+  | C_le
+  | L_par
+  | R_par
+  | EOF
+
+val main :
+  (Lexing.lexbuf  -> token) -> Lexing.lexbuf -> Expr.prog
