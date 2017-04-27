17332d0ea20a37493272391cf3ab3243b93b2177
gcluzel <guillaume.cluzel@ens-lyon.fr>
2017-04-27 11:07:05 +0200

    README

--- test/reference.ml
+++ test/reference.ml
@@ -1,9 +1,9 @@
 let a = ref 2 in
     if !a < 1 then
       (a := 4;
-       PrInt !a)
+       prInt !a)
     else
       (a := !a + 25;
-       4 - PrInt !a)
+       4 - prInt !a)
 ;;
   (* Exemple d'utilisation de la fonction PrInt *)
